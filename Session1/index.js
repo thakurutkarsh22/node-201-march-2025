48ec8e5c649485c35522387b96513cf9e94867f3
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2025-03-05 20:26:22 +0530

    session 1

---
+++ Session1/index.js
@@ -0,0 +1,47 @@
+const http = require("node:http");
+const PORT = 8083;
+
+
+const server  = http.createServer((req, res) => {
+
+    const url = req.url;
+
+    if(url === "/") {
+        // res.writeHead(200, { "content-type": "text/html" })
+        res.write("Welcome to the home page ");
+        res.write("hello utkarsh");
+        res.end();
+    } else if (url === "/abouts") {
+        // res.write();
+        res.end("This website is created by utkarsh, 8837463389"); // VV important line 
+    } else if (url === "/fitness") {
+        const fitnessObject = {
+            diet: "2kg",
+            height: 170,
+            running: 10,
+            sleep: true,
+            activities: ["cycling", "gym"],
+            cultAddress: {
+                houseNumber: "1910",
+                street: "HSR layour banglore"
+            },
+            createdDate: new Date().toLocaleDateString(),
+            createdTime: new Date().toLocaleTimeString(),
+        }
+
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(JSON.stringify(fitnessObject));
+    }
+
+
+    console.log("I have recieved request :: " , url)
+});
+
+
+server.listen(PORT, () => {
+    console.log("THUMBS UP the server is up at port number ", PORT)
+})
+
+
+
+
